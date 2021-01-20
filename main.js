5f8734377002a758305c44c9e6213a674e5d514f
piman <44853490+piman51277@users.noreply.github.com>
2021-01-20 11:44:45 -0500

    attempted fix for crash on heroku

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000 | process.env.PORT
+const path = require('path')
+const port = 3000 || process.env.PORT
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
 app.listen(port, () => {
     console.log(`Go to http://localhost:${port}`)
