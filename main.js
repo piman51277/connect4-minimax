fea396a9914b1f30fa589fad757d2a76cfbc952b
piman <44853490+piman51277@users.noreply.github.com>
2021-01-20 11:54:17 -0500

    attempt different approach

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000 || process.env.PORT
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
-})
-app.listen(port, () => {
-    console.log(`Go to http://localhost:${port}`)
-})
+app.use('/', express.static(__dirname + '/public'))
+app.listen(Number(process.env.PORT || 8000))
